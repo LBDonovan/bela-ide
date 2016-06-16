4c3016eba04645571c1aced1aece29f40f05a20c
Giulio Moro <giuliomoro@yahoo.it>
2016-06-16 17:33:55 +0100

    Using rsync when available, cleanly handles empty string at prompt

--- scripts/setup_IDE.sh
+++ scripts/setup_IDE.sh
@@ -38,32 +38,39 @@ shift $((OPTIND-1))
 # Find location of this script so we can locate the rest of the files
 SCRIPTDIR=$(dirname "$0")
 
-if [ $ALWAYS_YES -eq 0 ];
+printf "Warning: this script will DELETE any existing IDE files from your BeagleBone and install the IDE. Continue? (y/N) "
+
+if [ $ALWAYS_YES -eq 1 ];
 then
-  read -p "Warning: this script will DELETE any existing IDE files from your BeagleBone and install the IDE. Continue? (y/N) " -r
-else
-  REPLY=y
+  printf "y\n"
+else 
+  read REPLY;
+  [ -z $REPLY ] || { [ $REPLY !=  y ]  && [ $REPLY != Y ]; } && { echo "Aborting..."; exit 1; }
 fi
 
-if [[ $REPLY = y ]]
-then
-# Remove IDE files
-  printf "Removing old IDE files..." 
-  ssh $BBB_ADDRESS "rm -rf $BBB_PATH/IDE ; mkdir $BBB_PATH/IDE" &&\
-  printf "done\n" || { printf "\nError while removing the old files, is the board connected?\n"; exit 1; }
+
 # Copy relevant files to BeagleBone Black
-  printf "Copying new IDE files to BeagleBone..."
-  scp -rq $SCRIPTDIR/../IDE $BBB_ADDRESS:$BBB_PATH &&\
-  printf "done\n" || { printf "\nError while copying files: error $?\n"; exit 1; }
+printf "Copying new IDE files to BeagleBone..."
+[ -z $RSYNC_AVAILABLE ] && { [ -z `which rsync` ] && RSYNC_AVAILABLE=0 || RSYNC_AVAILABLE=1; }
+
+if [ $RSYNC_AVAILABLE -eq 1 ]
+then
+  printf "using rsync..."
+  rsync -ac --no-t --delete-after  $SCRIPTDIR/../IDE $BBB_ADDRESS:$BBB_PATH
+else
+  #if rsync is not available, let's clean the folder first
+  ssh $BBB_ADDRESS "rm -rf $BBB_PATH/IDE ; mkdir -p $BBB_PATH/IDE" || { printf "\nError while removing the old files, is the board connected?\n"; exit 1; }
+  printf "using scp...might take a while ..."
+  scp -rq $SCRIPTDIR/../IDE $BBB_ADDRESS:$BBB_PATH
+fi
+[ $? -eq 0 ] &&\
+printf "done\n" || { printf "\nError while copying files: error $?\n"; exit 1; }
 
 # Make sure the projects folder exists and there is a project in it
-  ssh $BBB_ADDRESS "cd $BBB_PATH/; mkdir -p projects/; [ -d projects/basic ] || cp -r examples/basic projects/" &&\
-  
+ssh $BBB_ADDRESS "cd $BBB_PATH/; mkdir -p projects/; [ -d projects/basic ] || cp -r examples/basic projects/" &&\
+
 # rebuild node dependencies
-  printf "Rebuilding node dependencies..."
-  ssh $BBB_ADDRESS "cd $BBB_PATH/IDE/; npm rebuild &>/dev/null" &&\
-  printf "done\n" || { printf "\nError while rebuilding dependencies.\n"; exit 1; }
-else
-  echo "Aborting..."
-fi
+printf "Rebuilding node dependencies..."
+ssh $BBB_ADDRESS "cd $BBB_PATH/IDE/; npm rebuild &>/dev/null" &&\
+printf "done\n" || { printf "\nError while rebuilding dependencies.\n"; exit 1; }
 
